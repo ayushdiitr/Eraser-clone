9ac93839149ce0df593c94333450434334937342
Ayush Dhiman <ayushd175@gmail.com>
2024-11-08 00:23:50 +0530

    add all files

---
+++ app/(routes)/dashboard/_components/Header.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@/components/ui/button';
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { Search, SendIcon } from 'lucide-react'
+import Image from 'next/image';
+import React from 'react'
+
+function Header() {
+
+    const {user} = useKindeBrowserClient();
+
+  return (
+    <div className='flex w-full justify-end gap-2 items-center pt-4' >
+        <div className='flex gap-2 items-center border rounded-md p-1'>
+            <Search className='h-4 w-4'/>
+            <input type='text' placeholder='Search' />
+        </div>
+        <div>
+            <Image src={user?.picture} alt='Profile' width={30} height={30} className='rounded-full ' />
+        </div>
+        <Button className='gap-2 flex h-8 hover:bg-blue-700 bg-blue-600 text-sm'>
+            <SendIcon className='h-4 w-4'/>
+            Invite
+        </Button>
+    </div>
+  )
+}
+
+export default Header
